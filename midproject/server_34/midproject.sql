4a597bb3fba98b2fabc0205e60662e362a87f987
A-iong <billy@BillydeMacBook-Pro.local>
2023-04-14 12:39:40 +0800

    新增 SQL 語法

---
+++ midproject/server_34/midproject.sql
@@ -0,0 +1,18 @@
+CREATE TABLE middle_product_34 (
+    pId int,
+    pName varchar(255),
+    price int,
+    cat_id varchar(255),
+    img_url varchar(255)
+)
+
+INSERT INTO middle_product_34
+VALUES
+(1, 'Coffee 1', 10, '1', 'https://coffee.alexflipnote.dev/lILFwuy_vrk_coffee.jpg'),
+(2, 'Coffee 2', 20, '1', 'https://coffee.alexflipnote.dev/Ww6RSmI76tg_coffee.jpg'),
+(3, 'Coffee 3', 3550, '1', 'https://coffee.alexflipnote.dev/rrgsJKE-i28_coffee.jpg'),
+(4, 'Coffee 4', 420, '1', 'https://coffee.alexflipnote.dev/_FvvZSjqHxI_coffee.jpg'),
+(5, 'Coffee 5', 530, '2', 'https://coffee.alexflipnote.dev/_YLhItYrXrk_coffee.jpg'),
+(6, 'Coffee 6', 640, '2', 'https://coffee.alexflipnote.dev/WJ23p4eMOS0_coffee.jpg'),
+(7, 'Coffee 7', 760, '2', 'https://coffee.alexflipnote.dev/qzXZnSk_9oU_coffee.jpg'),
+(8, 'Coffee 8', 870, '2', 'https://coffee.alexflipnote.dev/NCDllPSvXz4_coffee.png')
